6e2899571d5599ed26d4b332a5a3221d5cf5caea
Arthur Milchior <Arthur@Milchior.fr>
2019-09-22 20:22:40 +0200

    button in main window

---
+++ main.py
@@ -0,0 +1,12 @@
+from anki.hooks import addHook
+from aqt import mw
+from aqt.qt import QAction
+
+from .rule import updateAll
+
+addHook()
+
+action = QAction(aqt.mw)
+action.setText("Apply trigger->action rules")
+mw.form.menuTools.addAction(action)
+action.triggered.connect(updateAll)
